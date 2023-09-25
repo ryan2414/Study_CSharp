85a7b7e8933fc3c9e05c6b22bcf500d1d03f596f
이승재 <leeseungjae@jerry.local>
2023-09-25 22:08:23 +0900

    다형성

---
+++ 다형성/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace HelloWorld
+{
+    // 다형성
+    // 클래스 형식 변환
+    // 
+    class Player
+    {
+        protected int hp;
+        protected int attack;
+    }
+    class Knight : Player
+    {
+
+    }
+    class Mage : Player
+    {
+        public int mp;
+    }
+    class Program
+    {
+        static void EnterGame(Player player)
+        {
+            Mage mage2 = player as Mage;
+            if (mage2 != null)
+            {
+
+            }
+
+            bool isMage = player is Mage;
+            if (isMage)
+            {
+                Mage mage = (Mage)player;
+                mage.mp = 10;
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Knight knight = new Knight();
+            Mage mage = new Mage();
+
+            Player magePlayer = mage;
+            Mage mage2 = (Mage)magePlayer;
+
+            EnterGame(knight);
+            EnterGame(mage);
+        }
+    }
+}
