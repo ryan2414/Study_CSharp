764d3091d03abdb2700b425875a02ca37a594d07
이승재 <leeseungjae@jerry.local>
2023-09-25 21:53:44 +0900

    은닉성

---
+++ 은닉성/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace HelloWorld
+{
+    // OOP(은닉성/상속성/다형성)
+
+    class Knight
+    {
+        private int hp;
+
+        public void SetHp(int hp)
+        {
+            this.hp = hp;
+        }
+
+        public void SecretFunction()
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Knight knight = new Knight();
+            knight.SetHp(100);
+            knight.SecretFunction();
+        }
+    }
+}
