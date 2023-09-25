afad4a9daf53bbbdf5262d3090dc8569d7cc4833
이승재 <leeseungjae@jerry.local>
2023-09-26 06:58:30 +0900

    string

---
+++ String/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Harry Potter";
+            
+            // 1. 찾기
+            bool found = name.Contains("Harry");
+            int index = name.IndexOf('P');
+
+            // 2. 변형
+            name = name + " Junior";
+
+            string lowerName = name.ToLower();
+            string upperName = name.ToUpper();
+            string newName = name.Replace('r', 'l');
+
+            // 3. 분할
+            string[] names = name.Split(new char[] {' '});
+            string substringName = name.Substring(5);
+        }
+    }
+}
