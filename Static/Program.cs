ae44339be5e61fdcdad14593cb2693ed0bbfe090
이승재 <leeseungjae@jerry.local>
2023-09-25 21:33:49 +0900

    study_ Static, 상속성

---
+++ Static/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace HelloWorld
+{
+    // OOP(은닉성/상속성/다형성)
+
+    public class Player
+    {
+        public static int count; // 오로직 한개만 존재
+
+        public int id;
+        public int hp;
+        public int attack;
+
+        public Player()
+        {
+            Console.WriteLine("Player 생성자 호출");
+        }
+
+        public Player(int hp)
+        {
+            this.hp = hp;
+            System.Console.WriteLine($"{hp} Player 생성자 호출");
+        }
+
+        public void Move()
+        {
+            System.Console.WriteLine("Move");
+        }
+
+        public void Attack()
+        {
+            System.Console.WriteLine("Attack");
+
+        }
+
+    }
+
+    public class Mage : Player
+    {
+
+    }
+    public class Archer : Player
+    {
+
+    }
+    public class Knight : Player
+    {
+        int c;
+        public Knight() : base(100)
+        {
+            this.c = 10;
+            base.id = count;
+            count++;
+
+            hp = 100;
+            attack = 10;
+            Console.WriteLine("Knight 생성자 호출!");
+        }
+
+        public void Stun()
+        {
+            System.Console.WriteLine("Stun Attack");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Knight knight = new Knight();
+            knight.Move();
+        }
+    }
+}
--- Static/obj/Debug/net7.0/Static.AssemblyInfo.cs
+++ Static/obj/Debug/net7.0/Static.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Study_CSharp")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Static")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Study_CSharp")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Study_CSharp")]
+[assembly: System.Reflection.AssemblyProductAttribute("Static")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Static")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment 클래스에서 생성되었습니다.
