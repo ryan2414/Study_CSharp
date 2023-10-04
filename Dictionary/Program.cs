884c249066e5ead59d4decdc09b023d13b102fd2
이승재 <leeseungjae@jerry.local>
2023-10-04 21:34:02 +0900

    공부f

---
+++ Dictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    class Monster
+    {
+        public int id;
+        public Monster(int id)
+        {
+            this.id = id;
+        }
+
+    }
+    static void Main(string[] args)
+    {
+        // HashTable
+        // Key -> Value
+
+        Dictionary<int, Monster> dic = new Dictionary<int, Monster>();
+        for (int i = 0; i < 10000; i++)
+        {
+            dic.Add(i, new Monster(i));
+        }
+
+        Monster mon;
+        bool found = dic.TryGetValue(5000, out mon);
+
+        dic.Remove(777);
+    }
+}
---
+++ List/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // List <- 동적 배열
+        List<int> list = new List<int>();
+        for (int i = 0; i < 5; i++)
+        {
+            list.Add(i);
+        }
+
+        // 삽입 삭제
+        list.Insert(2, 999);
+        
+        bool sucess = list.Remove(999);
+        list.RemoveAt(2);
+
+        foreach (var i in list)
+        {
+            System.Console.WriteLine(i);
+        }
+    }
+}
