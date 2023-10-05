d146519d73eced35f44f3736fcc64faec1e7e4de
이승재 <leeseungjae@jerry.local>
2023-10-05 22:12:42 +0900

    Property

---
+++ Property/Program/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    // 객체지향 -> 은닉성
+    // 불필요한 정보를 노출하지 않음
+
+    class Knight
+    {
+        private int hp;
+
+        public int HP
+        {
+            get { return hp; }
+            set { this.hp = value; }
+        }
+
+        public int HP2 { get; private set; } = 100;
+
+        // Getter Get함수
+        public int GetHp() { return hp; }
+        // Setter Set함수
+        public void SetHp(int hp) { this.hp = hp; }
+    }
+
+    static void Main(string[] args)
+    {
+        Knight knight = new Knight();
+        knight.HP = 100;
+        int hp = knight.HP;
+    }
+}
