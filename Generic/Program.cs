5739bfe50f70722e0710faf78df360577884900b
이승재 <leeseungjae@jerry.local>
2023-10-05 20:57:40 +0900

    공부

---
+++ Generic/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    class MyList<T> where T : struct
+    {
+        T[] arr = new T[10];
+
+        public T GetItem(int i)
+        {
+            return arr[i];
+        }
+    }
+
+    static void Test<T>(T input)
+    {
+
+    }
+    
+    static void Main(string[] args)
+    {
+        MyList<int> myIntList = new MyList<int>();
+        int item = myIntList.GetItem(0);
+
+
+        MyList<short> myShortList = new MyList<short>();
+
+        Test<int>(3);
+    }
+}
