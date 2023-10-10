4e20d72b1e29a519255dbe80e865b67d105b07ce
이승재 <leeseungjae@jerry.local>
2023-10-11 08:07:58 +0900

    study

---
+++ Exception/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    class TestException : Exception
+    {
+
+    }
+    static void Main(string[] args)
+    {
+        try
+        {
+            //1. 0으로 나눌 때
+            //2. 잘못된 메모리를 참조(null)
+            //3. 오버플로우
+            
+            // int a = 10;
+            // int b = 0;
+            // int result = a / b;
+
+            throw new TestException();
+        }
+        catch (DivideByZeroException e)
+        {
+            System.Console.WriteLine(e);
+        }
+        catch (Exception e)
+        {
+            System.Console.WriteLine(e);
+        }
+        finally
+        {
+            //DB, 파일 정리 등등
+
+        }
+    }
+}
---
+++ Reflection/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+
+class Program
+{
+    class Important : System.Attribute
+    {
+        string message;
+        public Important(string message) { this.message = message; }
+    }
+    class Monster
+    {
+        [Important("Very Important")]//컴퓨터가 런타임에 참조할 수 있는 주석
+        public int hp;
+        protected int attack;
+        private float speed;
+    
+        void Attack() { }
+    }
+    static void Main(string[] args)
+    {
+        // Reflection: X-ray
+        Monster monster = new Monster();
+        Type type = monster.GetType();
+
+        var fields = type.GetFields(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Instance);
+
+        foreach (FieldInfo field in fields)
+        {
+            string access = "protected";
+            if (field.IsPublic)
+                access = "public";
+            else if (field.IsPrivate)
+                access = "private";
+
+            var ATT = field.GetCustomAttributes();
+            System.Console.WriteLine($"{access} {field.FieldType.Name} {field.Name}");
+
+        }
+    }
+}
