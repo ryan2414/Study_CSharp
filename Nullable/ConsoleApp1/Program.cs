1f605d1978a639a4b6780588fda62c65414e6f75
이승재 <leeseungjae@jerry.local>
2023-10-12 21:23:30 +0900

    Nullable

---
+++ Nullable/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    class Monster
+    {
+        public int Id {get; set;}
+    }
+    static void Main(string[] args)
+    {
+        // Nullable -> Null + able
+        int? number = null;
+
+        int b = number ?? 0;
+
+        //number = 3;
+
+        if (number.HasValue)
+        {
+            int a = number.Value;
+            System.Console.WriteLine(a);
+        }
+
+        Monster monster = null;
+        
+        if (monster != null)
+        {
+            int monsterId = monster.Id;
+        }
+
+        int? id = monster?.Id;
+
+    }
+}
